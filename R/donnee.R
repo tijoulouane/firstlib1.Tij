5d70cf4f8b9fe551682a8a5d8e0077cf02fcd65b
Louane <tijoulouane@gmail.com>
2026-04-15 21:35:05 +0200

    description donnée

---
+++ R/donnee.R
@@ -0,0 +1,13 @@
+#' Jeu de données df_velo
+#'
+#' Données utilisées pour l'entraînement et l'apprentissage de la création
+#' d'un package R.
+#'
+#' @format A data frame with XX rows and XX variables:
+#' \describe{
+#'   \item{var1}{Description de la variable 1}
+#'   \item{var2}{Description de la variable 2}
+#'   \item{var3}{Description de la variable 3}
+#' }
+#' @source Fichier de données importé dans le package.
+"donnee"
